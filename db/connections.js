fd89363f191b9eb76a935222d6eff4bcf261e989
john-330 <boss_jaxon@mail.ru>
2022-04-05 19:04:07 -0500

    deploy

--- db/connections.js
+++ db/connections.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const mongoURI = 'mongodb://localhost:27017/fleettab'
 const MONGODB_URI = process.env.MONGODB_URI
 
-mongoose.connect(mongoURI, {
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
